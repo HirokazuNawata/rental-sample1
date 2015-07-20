c7a3922b79a06659c2fb0957ece35e821a76862b
Tomo Masakura <fj0706kr@aa.jp.fujitsu.com>
2015-07-20 14:37:38 +0900

    レンタル料を計算するオブジェクトを抽出

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,6 +11,41 @@
  */
 angular.module('app6App')
   .controller('MainCtrl', function ($scope) {
+    var rentalService = {
+      getRentalFee: function (item, days) {
+        var amount = 0;
+
+        if (item.type === 'new') {
+          // 新作
+
+          // 一日 300 円
+          amount += days * 300;
+        } else if (item.type === 'regular') {
+          // 旧作
+
+          // 最初の二日までは 200 円
+          amount += 200;
+
+          // 三日目からは一日ごとに 150 円
+          if (days > 2) {
+            amount += (days - 2) * 150;
+          }
+        } else if (item.type === 'kids') {
+          // 子供向け
+
+          // 最初の三日までは 150 円
+          amount += 150;
+
+          // 四日目からは一日 150 円
+          if (days > 3) {
+            amount += (days - 3) * 150;
+          }
+        }
+
+        return amount;
+      }
+    };
+
     $scope.days = 1;
     $scope.items = [
       {id: 'v2001', title: 'デス・ノート (実写)', type: 'new'},
@@ -26,34 +61,8 @@ angular.module('app6App')
           return item.id === $scope.selectedItem;
         })[0];
 
-      var amount = 0;
-
-      if (item.type === 'new') {
-        // 新作
-
-        // 一日 300 円
-        amount += $scope.days * 300;
-      } else if (item.type === 'regular') {
-        // 旧作
-
-        // 最初の二日までは 200 円
-        amount += 200;
-
-        // 三日目からは一日ごとに 150 円
-        if ($scope.days > 2) {
-          amount += ($scope.days - 2) * 150;
-        }
-      } else if (item.type === 'kids') {
-        // 子供向け
-
-        // 最初の三日までは 150 円
-        amount += 150;
-
-        // 四日目からは一日 150 円
-        if ($scope.days > 3) {
-          amount += ($scope.days - 3) * 150;
-        }
-      }
+      // レンタル料を計算する
+      var amount = rentalService.getRentalFee(item, $scope.days);
 
       // サーバーへ書き込んでると思ってください
       alert(amount + ' 円です');
